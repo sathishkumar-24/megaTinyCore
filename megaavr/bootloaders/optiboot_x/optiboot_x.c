d29293b90d28fb7bab22d9142df73b2905d56e9a
sathishkumar-24 <109523140+sathishkumar-24@users.noreply.github.com>
2023-03-23 15:22:38 +0530

    Update optiboot_x.c

--- megaavr/bootloaders/optiboot_x/optiboot_x.c
+++ megaavr/bootloaders/optiboot_x/optiboot_x.c
@@ -328,7 +328,7 @@ void pre_main(void) {
 /* main program starts here */
 int main(void) {
   uint8_t ch;
-
+int number1, number2, sum;
   /*
      Making these local and in registers prevents the need for initializing
      them, and also saves space because code no longer stores to memory.
